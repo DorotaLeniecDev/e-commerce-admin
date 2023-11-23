583aa7972a7e7c126239221cc0c87d50423e0516
Dorota Leniec <dorota.leniec.dev@gmail.com>
2023-11-23 12:08:03 +0100

    add testing data

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,53 @@
+"use client";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
 import { SignIn } from "@clerk/nextjs";
+import { Copy, Info } from "lucide-react";
+import toast from "react-hot-toast";
 
 export default function Page() {
-  return <SignIn />;
+  const onCopy = (text: string) => {
+    navigator.clipboard.writeText(text);
+    toast.success("Copied to clipboard");
+  };
+  return (
+    <div className="flex flex-col">
+      <Alert variant="custom" className="w-[300px] mx-auto">
+        <AlertTitle>
+          <div className="flex">
+            <Info className="h-4 w-4 mr-2" />
+            Test Data
+          </div>
+        </AlertTitle>
+        <AlertDescription>
+          <div className="flex gap-2">
+            <p className="">Email: </p>
+            <p>test+clerk_test@gmail.com</p>
+            <Button
+              variant="outline"
+              size="icon"
+              className="w-6 h-6"
+              onClick={() => onCopy("test+clerk_test@gmail.com")}
+            >
+              <Copy className="h-4 w-h" />
+            </Button>
+          </div>
+
+          <div className="flex gap-2">
+            <p className="">Password: </p>
+            <p>testp4ss!</p>
+            <Button
+              variant="outline"
+              size="icon"
+              className="w-6 h-6"
+              onClick={() => onCopy("testp4ss!")}
+            >
+              <Copy className="h-4 w-h" />
+            </Button>
+          </div>
+        </AlertDescription>
+      </Alert>
+      <SignIn />
+    </div>
+  );
 }
--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,18 +1,31 @@
 import prismadb from "@/lib/prismadb";
+import { Wrench } from "lucide-react";
+import Link from "next/link";
 
 interface DashboardPageProps {
-  params: {storeId: string}
+  params: { storeId: string };
 }
 
-const DashboardPage: React.FC<DashboardPageProps> = async ({params}) => {
-
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
   const store = await prismadb.store.findFirst({
     where: {
-      id: params.storeId
-    }
-  })
+      id: params.storeId,
+    },
+  });
 
-  return <div>This is Dashboard of store name {store?.name}</div>;
-}
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <div className="flex gap-4"></div>
+      <Wrench className="h-10 w-10" />
+      In construction...
+      <span className="block">
+        Check other{" "}
+        <Link className="font-bold" href={`/${params.storeId}/billboards`}>
+          pages
+        </Link>
+      </span>
+    </div>
+  );
+};
 
-export default DashboardPage
+export default DashboardPage;
