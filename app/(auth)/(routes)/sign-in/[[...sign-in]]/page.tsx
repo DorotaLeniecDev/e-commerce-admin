5b0972064054eb8d62adeddec74e1358971c0e75
Dorota Leniec <dorota.leniec.dev@gmail.com>
2023-11-27 19:52:49 +0100

    add basic loading page:

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -11,35 +11,35 @@ export default function Page() {
     toast.success("Copied to clipboard");
   };
   return (
-    <div className="flex flex-col">
-      <Alert variant="custom" className="w-[300px] mx-auto">
+    <div className="flex flex-row">
+      <Alert variant="custom" className="w-[300px] mx-auto h-[100px]">
         <AlertTitle>
-          <div className="flex">
-            <Info className="h-4 w-4 mr-2" />
+          <div className="flex dark:text-black">
+            <Info className="h-4 w-4 mr-2 " />
             Test Data
           </div>
         </AlertTitle>
         <AlertDescription>
-          <div className="flex gap-2">
+          <div className="flex gap-2 dark:text-black">
             <p className="">Email: </p>
             <p>test+clerk_test@gmail.com</p>
             <Button
               variant="outline"
               size="icon"
-              className="w-6 h-6"
+              className="w-6 h-6 dark:text-white"
               onClick={() => onCopy("test+clerk_test@gmail.com")}
             >
               <Copy className="h-4 w-h" />
             </Button>
           </div>
 
-          <div className="flex gap-2">
+          <div className="flex gap-2 dark:text-black">
             <p className="">Password: </p>
             <p>testp4ss!</p>
             <Button
               variant="outline"
               size="icon"
-              className="w-6 h-6"
+              className="w-6 h-6 dark:text-white"
               onClick={() => onCopy("testp4ss!")}
             >
               <Copy className="h-4 w-h" />
